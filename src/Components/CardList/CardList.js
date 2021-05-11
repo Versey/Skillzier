e334bf91499016c1496ab451674c55a851cbb5f2
Michael Leroux <mike_leroux121@hotmail.com>
2021-05-11 00:57:00 -0400

    Commit
    Another commit

--- src/Components/CardList/CardList.js
+++ src/Components/CardList/CardList.js
@@ -1,9 +1,20 @@
 import Grid from '@material-ui/core/Grid';
+import MyCard from './'
 
 const GridList = props =>
 {
 
+return(
+    <div className="card-list">
+        <Grid>
 
+            
+        </Grid>
+    
+    
+    </div>
+
+);
 
 }
 
